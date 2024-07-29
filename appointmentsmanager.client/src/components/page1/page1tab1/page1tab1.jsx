7410676403a8168767676c7cab33170d49c97706
Microsoft full stack <microfullstack@outlook.com>
2024-07-29 10:06:07 -0700

    sample columns added

--- appointmentsmanager.client/src/components/page1/page1tab1/page1tab1.jsx
+++ appointmentsmanager.client/src/components/page1/page1tab1/page1tab1.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './page1tab1.css'
 
 export const Page1Tab1 = () => {
-    const columns = ['Name', 'Age', 'City'];
+    const columns = ['Name', 'Age', 'City','Name1','Age1','City1','Name2','Age2','City2','Name3','Age3','City3'];
     const data = [
-        { id: 1, name: 'Alice', age: 25, city: 'New York' },
-        { id: 2, name: 'Bob', age: 30, city: 'Los Angeles' },
-        { id: 3, name: 'Charlie', age: 28, city: 'Chicago' },
-        { id: 4, name: 'David', age: 35, city: 'Houston' },
+        { id: 1, name: '', age: 25, city: '', name1: '', age1: 25, city1: '', name2: '', age2: 25, city2: '', name3: '', age3: 25, city3: '' },
+        { id: 2, name: '', age: 30, city: '', name1: '', age1: 25, city1: '', name2: '', age2: 25, city2: '', name3: '', age3: 25, city3: '' },
+        { id: 3, name: '', age: 28, city: '', name1: '', age1: 25, city1: '', name2: '', age2: 25, city2: '', name3: '', age3: 25, city3: '' },
+        { id: 4, name: '', age: 35, city: '', name1: '', age1: 25, city1: '', name2: '', age2: 25, city2: '', name3: '', age3: 25, city3: '' },
     ];
     return (
         < p>
@@ -28,6 +28,15 @@ export const Page1Tab1 = () => {
                                 <td>{row.name}</td>
                                 <td>{row.age}</td>
                                 <td>{row.city}</td>
+                                <td>{row.name1}</td>
+                                <td>{row.age1}</td>
+                                <td>{row.city1}</td>
+                                <td>{row.name2}</td>
+                                <td>{row.age2}</td>
+                                <td>{row.city2}</td>
+                                <td>{row.name3}</td>
+                                <td>{row.age3}</td>
+                                <td>{row.city3}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -36,3 +45,5 @@ export const Page1Tab1 = () => {
     </p >
     );
 }
+
+
